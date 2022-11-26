7667f884ea3baae2ef70da77225015cf8a9ea60d
Имыкшенов Геннадий <63225192+imykshenov@users.noreply.github.com>
2022-11-27 06:13:07 +0800

    Update sql.sql

--- sql.sql
+++ sql.sql
@@ -1,1 +1,6 @@
-﻿SELECT * FROM products LEFT JOIN category ON products.SectorID = category.SectorID;
+SELECT P."Name", C."Name"
+FROM Products P
+LEFT JOIN ProductCategories PC
+	ON P.Id = PC.ProductId
+LEFT JOIN Categories C
+	ON PC.CategoryId = C.Id;
